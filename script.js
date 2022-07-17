305af83d1b21046e434e40f8a2b0024ae78ffb9f
SL programming:git config --global user.name SL <you@example.com>
2022-07-17 09:48:11 +0630

    added cross and circle

--- script.js
+++ script.js
@@ -2,10 +2,17 @@ console.log("hello world")
 const boxs = document.querySelectorAll(".box");
 const overlayTag = document.querySelector(".overLay");
 const winnerMessage = document.querySelector(".winnerMessage");
+const drawTag = document.getElementsByClassName("winner")[0];
 const winnerTag = document.getElementsByClassName("winner")[1];
 const buttonsTag = document.querySelector(".buttons");
 const playAgainTag = document.querySelector(".playAgain");
-
+const signTAg = document.querySelector(".sign");
+const circle = `<div class="circle"></div>`;
+const cross = `<div class="cross">
+    <div class="bar1 bars"></div>
+    <div class="bar2 bars"></div>
+    </div>`;
+let isDraw=false;
 let winner;
 let finish=false;
 let id2 = [];
@@ -16,10 +23,12 @@ let player1Count =0;
 let player2Count =0;
 let selectedByPlayer1;
 let selectedByPlayer2;
+
 for (let box of boxs) {
 
     box.id=boxId;
     boxId++;
+    
     box.addEventListener("click" , () => {
         if(box.classList.contains("mark")) {
             return;
@@ -27,6 +36,7 @@ for (let box of boxs) {
         if(!isFirstPlayerTurn) {
             box.classList.add("mark","player2");
             player2Count++;
+            box.innerHTML=cross;
             selectedByPlayer2 = document.getElementsByClassName("player2");
             for (let item of selectedByPlayer2) {
                 
@@ -38,11 +48,13 @@ for (let box of boxs) {
                 
             }
             GameObject(id2,"player2");
+            draw();
             finishF("player2");
             isFirstPlayerTurn=true;
         }else {
             box.classList.add("mark","player1");
             player1Count++;
+            box.innerHTML=circle;
             selectedByPlayer1 = document.getElementsByClassName("player1");
             isFirstPlayerTurn=false;
             for (let item of selectedByPlayer1) {
@@ -55,6 +67,7 @@ for (let box of boxs) {
                 
             }
             GameObject(id1,"player1");
+            draw();
             finishF("player1");
         }
        
@@ -91,26 +104,43 @@ const GameObject = (ids,player) => {
 }
 const finishF = (player) => {
     if (finish) {
+        drawTag.innerHTML="Winner is";
         overlayTag.style.display="block";
         winnerTag.append(player);
-        playAgainTag.addEventListener("click",() => {
-            playAgainFunction("player1");
-            playAgainFunction("player2");
-            overlayTag.style.display="none";
-            winnerTag.innerHTML="";
-        })
+        if (player=="player1") {
+            isFirstPlayerTurn=false;
+        }else {
+            isFirstPlayerTurn=true;
+        }
+        
+    }else if (isDraw) {
+        drawTag.innerHTML="DRAW!";
+        overlayTag.style.display="block";
     }
+    playAgainTag.addEventListener("click",() => {
+        playAgainFunction();
+        overlayTag.style.display="none";
+        winnerTag.innerHTML="";
+    })
 }
 const playAgainFunction = () => {
     id2 = [];
     id1 = [];
+    player1Count=0;
+    player2Count=0;
     finish=false;
+    isDraw=false;
     for (i=1;i<10;i++) {
         const boxsTags = document.getElementById(i);
+        boxsTags.innerHTML="";
         boxsTags.classList.remove("mark");
         boxsTags.classList.remove("player1");
         boxsTags.classList.remove("player2");
     }
     
 }
-
+const draw = () => {
+        if (!finish && player1Count==5 || player2Count==5) {
+            isDraw = true;
+        }
+}
